3c23c132afff4aa121abbbd1e0cc79eff3384167
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-07-10 10:37:13 +0530

    Updated in viva

--- Q2.scala
+++ Q2.scala
@@ -1,7 +1,13 @@
-def PatternMatching(n : Int) : String = {
-    n match {
-        case x if x <= 0 => "Negative/Zero is input"
-        case x if x % 2 == 0 => "Even number is given"
-        case _ => "Odd number is given"
+object pattternmatch {
+    def PatternMatching(n : Int) : String = {
+        n match {
+            case x if x <= 0 => "Negative/Zero is input"
+            case x if x % 2 == 0 => "Even number is given"
+            case _ => "Odd number is given"
+        }
+    }
+
+    def main (args : Array[String]) : Unit = {
+        println(PatternMatching(0))
     }
 }
